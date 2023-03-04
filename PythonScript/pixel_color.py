011d09541a93f915372575bec30f9091cf4964ab
Ken Chan <echan123@uottawa.ca>
2023-03-04 14:57:28 -0500

    Add call to TheColor API

--- PythonScript/pixel_color.py
+++ PythonScript/pixel_color.py
@@ -5,11 +5,11 @@
 # import image
 from PIL import Image #python image library
 import numpy as np
+import requests
 
 #create image obj and open for reading
 
 im = Image.open("C:\\Users\\kkiti\\OneDrive\\Documents\\LekipMorisien\\PythonScript\\3.jpg", 'r')
-
 
 #extract pixels from the image into a list
 #starting left to right top to bottom
@@ -27,5 +27,9 @@ print(image_pixels)
 print("\n")
 print("MIDDLE PIXEL HERE!!!\n")
 print(middle_pixel)
+
+RGB = ",".join([str(value) for value in middle_pixel])
+response = requests.get("https://www.thecolorapi.com/id", params = {"rgb": RGB})
+print(response.json()["name"]["value"])
 
 # problem: we cannot map this pixel to this color need to use an image library that isnt working so i gave up lol
